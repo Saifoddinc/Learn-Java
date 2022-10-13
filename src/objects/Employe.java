7114e059e654e9c667a673cfdb171ad779e76b8b
Sana <momin.sana24@gmail.com>
2022-10-13 23:59:15 +0530

    Dept.

--- src/objects/Employe.java
+++ src/objects/Employe.java
@@ -4,6 +4,7 @@ public class Employe {
 
 	String empName;
 	int empid;
+	String dept;
 	
 		void setName() {
 			System.out.println("emp name " + empName);
@@ -11,13 +12,18 @@ public class Employe {
 		void getEmpid() {
 			System.out.println("emp id " + empid);
 		}
+		void setdept() {
+			System.out.println("department " + dept);
+		}
 		 public static void main(String[] args) {
 				Employe obj1 = new Employe();
 				obj1.empName="saifoddin";
 				obj1.empid = 123;
+				obj1.dept = "Finance";
+				
 				obj1.setName();
 				obj1.getEmpid();
-						
+				obj1.setdept();		
 			}
 
 		
